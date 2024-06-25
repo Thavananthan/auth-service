36cc0a41d89a1ddca770028744a13c64173f6da4
Thavananthan <nnanthu49@gmail.com>
2024-06-25 15:15:50 +0530

    chnages added

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { verifyGatewayRequest } from '@thavananthan/eapp-shared';
 import { authRoutes } from '@auth/routes/auth';
 import { currentUserRoutes } from '@auth/routes/current-user';
 import { healthRoutes } from '@auth/routes/health';
+import { searchRoutes } from '@auth/routes/search';
+import { seedRoutes } from '@auth/routes/seed';
 
 const BASE_PATH = '/api/v1/auth';
 
@@ -10,4 +12,6 @@ export function appRoutes(app: Application): void {
   app.use('', healthRoutes());
   app.use(BASE_PATH, verifyGatewayRequest, authRoutes());
   app.use(BASE_PATH, verifyGatewayRequest, currentUserRoutes());
+  app.use(BASE_PATH, searchRoutes());
+  app.use(BASE_PATH, seedRoutes());
 }
